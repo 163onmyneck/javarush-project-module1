488dfc5367e5617a59677da33a1ddcedfae08ca7
Oleh <mihailyakyb45@gmail.com>
2023-11-03 13:58:28 +0200

    added 2 methods in CaesarCipher

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -1,4 +1,14 @@
 package com.javarush;
 
+import com.javarush.cipher.CaesarCipher;
+
 public class Main {
+    public static void main(String[] args) {
+        CaesarCipher cipher = new CaesarCipher();
+        String s = "««««««asdfkbs v scvsdvsfsdf::: asdasd";
+        int key = 1;
+        System.out.println(cipher.cryptForEngLanguage(s, key));
+        String ss = "фіфівіфвпкуеавимпва:??";
+        System.out.println(cipher.cryptForUaLanguage(ss, key));
+    }
 }
--- src/main/java/com/javarush/cipher/CaesarCipher.java
+++ src/main/java/com/javarush/cipher/CaesarCipher.java
@@ -1,8 +1,57 @@
 package com.javarush.cipher;
 
+import com.javarush.constants.Constants;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CaesarCipher {
 
-    public String caesarCipher(String string){
-        return string;
+    public static ArrayList<Character> getCharsFromString(String string) {
+        char[] chars = string.toCharArray();
+        ArrayList<Character> characterArrayList = new ArrayList<>();
+        for (char c : chars) {
+            characterArrayList.add(c);
+        }
+        return characterArrayList;
+    }
+
+    public String cryptForEngLanguage(String string, int key) {
+        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList<Character> chars = CaesarCipher.getCharsFromString(string);
+        ArrayList<Character> engChars = Constants.getAlphabeticEn();
+        for (int i = 0; i < chars.size(); i++) {
+            int index = engChars.indexOf(chars.get(i));
+            if (index != -1) {
+                index = (index + key) % engChars.size();
+                if (index < 0) {
+                    index += engChars.size();
+                }
+                stringBuilder.append(engChars.get(index));
+            } else {
+                stringBuilder.append(chars.get(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public String cryptForUaLanguage(String string, int key) {
+        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList<Character> chars = CaesarCipher.getCharsFromString(string);
+        ArrayList<Character> uaChars = Constants.getAlphabeticUa();
+        for (int i = 0; i < chars.size(); i++) {
+            int index = uaChars.indexOf(chars.get(i));
+            if (index != -1) {
+                index = (index + key) % uaChars.size();
+                if (index < 0) {
+                    index += uaChars.size();
+                }
+                stringBuilder.append(uaChars.get(index));
+            } else {
+                stringBuilder.append(chars.get(i));
+            }
+        }
+        return stringBuilder.toString();
     }
 }
