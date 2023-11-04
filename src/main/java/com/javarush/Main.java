79b0b8e0c4920503740d80ba8e1da4fc3498e0db
Oleh <mihailyakyb45@gmail.com>
2023-11-04 10:46:24 +0200

    added decrypt methods

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -5,10 +5,17 @@ import com.javarush.cipher.CaesarCipher;
 public class Main {
     public static void main(String[] args) {
         CaesarCipher cipher = new CaesarCipher();
-        String s = "««««««asdfkbs v scvsdvsfsdf::: asdasd";
-        int key = 1;
-        System.out.println(cipher.cryptForEngLanguage(s, key));
+        int key = 3;
         String ss = "фіфівіфвпкуеавимпва:??";
+        System.out.println(ss);
         System.out.println(cipher.cryptForUaLanguage(ss, key));
+        String result = cipher.cryptForUaLanguage(ss, key);
+        System.out.println(cipher.decryptForUaLanguage(result, key));
+
+        String s = "««««««asdfkbs v scvsdvsfsdf::: asdasd";
+        System.out.println(s);
+        String result1 = cipher.cryptForEngLanguage(s, key);
+        System.out.println(result1);
+        System.out.println(cipher.decryptForEngLanguage(result1, key));
     }
 }
--- src/main/java/com/javarush/cipher/CaesarCipher.java
+++ src/main/java/com/javarush/cipher/CaesarCipher.java
@@ -44,4 +44,42 @@ public class CaesarCipher {
         }
         return stringBuilder.toString();
     }
+
+    public String decryptForUaLanguage(String string, int key) {
+        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList<Character> chars = StringUtil.getCharsFromString(string);
+        ArrayList<Character> uaChars = Constants.getAlphabeticUa();
+        for (int i = 0; i < chars.size(); i++) {
+            int index = uaChars.indexOf(chars.get(i));
+            if (index != -1) {
+                index = (index - key) % uaChars.size();
+                if (index < 0) {
+                    index += uaChars.size();
+                }
+                stringBuilder.append(uaChars.get(index));
+            } else {
+                stringBuilder.append(chars.get(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public String decryptForEngLanguage(String string, int key) {
+        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList<Character> chars = StringUtil.getCharsFromString(string);
+        ArrayList<Character> engChars = Constants.getAlphabeticEn();
+        for (int i = 0; i < chars.size(); i++) {
+            int index = engChars.indexOf(chars.get(i));
+            if (index != -1) {
+                index = (index - key) % engChars.size();
+                if (index < 0) {
+                    index += engChars.size();
+                }
+                stringBuilder.append(engChars.get(index));
+            } else {
+                stringBuilder.append(chars.get(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
 }
