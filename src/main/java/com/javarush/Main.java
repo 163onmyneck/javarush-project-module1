6c9fb57b5dd511aa8f063b1635305377d4e50664
Oleh <mihailyakyb45@gmail.com>
2023-11-09 14:08:01 +0200

    added .jar file

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -6,10 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Runner runner = new Runner();
         runner.run();
-
-        System.out.println("Comand: " + args[0]);
-        System.out.println("Path to file: " + args[1]);
-        System.out.println("key: " + args[2]);
     }
 }
 
